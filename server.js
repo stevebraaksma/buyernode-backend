b9b99ffd6d2fe50812de08fb0f502cde67ba5fe3
Steve Braaksma <stevebraaksma@hotmail.com>
2021-10-26 22:07:50 -0500

    connect to mongodb

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 
 
 require('dotenv').config();
-const { PORT = 3001 } = process.env;
+const { PORT = 3001, DATABASE_URL } = process.env;
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+
+mongoose.connect(DATABASE_URL);
+mongoose.connection
+    .on('open', () => console.log("connected to MongoDB"))
+    .on('close', () => console.log('disconnected from MongoDB'))
+    .on('error', (error) => console.log(error));
+
+
+
+
+// ROUTES
 // test route
 app.get('/', (req, res) => {
     res.send('hello');
